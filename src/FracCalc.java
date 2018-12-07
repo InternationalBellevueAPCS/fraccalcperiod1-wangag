c12c7214cf091b66316635a784d3fb7641236af6
s-wangale <s-wangale@49S50024534.it.bsd405.org>
2018-12-07 01:38:59 -0800

    Completed checkpoint 2.

--- src/FracCalc.java
+++ src/FracCalc.java
@@ -6,15 +6,19 @@ public class FracCalc {
      * Prompts user for input, passes that input to produceAnswer, then outputs the result.
      * @param args - unused
      */
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         // TODO: Read the input from the user and call produceAnswer with an equation
         // Checkpoint 1: Create a Scanner, read one line of input, pass that input to produceAnswer, print the result.
         Scanner console = new Scanner(System.in);
         System.out.print("input: ");
         String input = console.nextLine();
-        String answer = produceAnswer(input);
-        System.out.println(answer);
+        
+        while (!input.equals("quit")) {
+        	String answer = produceAnswer(input);
+            System.out.println(answer);
+        	System.out.print("input: ");
+            input = console.nextLine();
+        }
         console.close();
     	// Checkpoint 2: Accept user input multiple times.
     }
@@ -26,16 +30,16 @@ public class FracCalc {
      * @return the result of the fraction after it has been calculated.
      *      Example: return ==> "1_1/4"
      */
-    public static String produceAnswer(String input)
-    { 
-        // TODO: Implement this function to produce the solution to the input
-        // Checkpoint 1: Return the second operand.  Example "4/5 * 1_2/4" returns "1_2/4".
+    public static String produceAnswer(String input) {
     	Scanner sc = new Scanner(input);
     	String operand1 = sc.next();
     	String operator = sc.next();
     	String operand2 = sc.next();
     	sc.close();
-    	return operand2;
+    	
+    	String parts1 = parseOperand(operand1);
+    	String parts2 = parseOperand(operand2);
+    	
         // Checkpoint 2: Return the second operand as a string representing each part.
         //               Example "4/5 * 1_2/4" returns "whole:1 numerator:2 denominator:4".
         // Checkpoint 3: Evaluate the formula and return the result as a fraction.
@@ -44,10 +48,31 @@ public class FracCalc {
         // Final project: All answers must be reduced.
         //               Example "4/5 * 1_2/4" returns "1_1/5".
         
-        //return "";
+        return parts2;
     }
-
-    // TODO: Fill in the space below with helper methods
+    
+    public static String parseOperand(String operand) {
+    	String whole = "0";
+    	String numerator = "0";
+    	String denominator = "1";
+    	for (int i = 0; i < operand.length(); i++) {
+    		if (operand.charAt(i) == '_') {
+    			whole = operand.substring(0, i);
+    		}
+    		if (operand.charAt(i) == '/') {
+    			if (operand.indexOf('_') != -1) {
+    				numerator = operand.substring(operand.indexOf('_') + 1, i);
+    			} else {
+    				numerator = operand.substring(0, i);
+    			}
+    			denominator = operand.substring(i+1);
+    		}
+    	}
+    	if (operand.indexOf('/') == -1) {
+    		whole = operand;
+    	}
+    	return "whole:" + whole + " numerator:" + numerator + " denominator:" + denominator;
+    }
     
     /**
      * greatestCommonDivisor - Find the largest integer that evenly divides two integers.
@@ -57,8 +82,7 @@ public class FracCalc {
      * @param b - Second integer.
      * @return The GCD.
      */
-    public static int greatestCommonDivisor(int a, int b)
-    {
+    public static int greatestCommonDivisor(int a, int b) {
         a = Math.abs(a);
         b = Math.abs(b);
         int max = Math.max(a, b);
@@ -78,8 +102,7 @@ public class FracCalc {
      * @param b - Second integer.
      * @return The LCM.
      */
-    public static int leastCommonMultiple(int a, int b)
-    {
+    public static int leastCommonMultiple(int a, int b) {
         int gcd = greatestCommonDivisor(a, b);
         return (a*b)/gcd;
     }
